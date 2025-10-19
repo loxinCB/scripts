2be0d4777ff14373b7340a05cd3116d1e1218e06
Marcel Fahning <marcel@fahning.me>
2025-10-19 18:15:13 +0200

    added errorhandling

--- debian_subv.sh
+++ debian_subv.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-VOLUME_GROUP_NAME=VG0-LV0
+VOLUME_GROUP_NAME=$(mount | grep /@rootfs | awk '{print $1}')
+echo root mount is $VOLUME_GROUP_NAME
 # get boot and efi partitions
 boot=$(mount | grep /target/boot | awk '{print $1}')
 efi=$(mount | grep /target/boot/efi | awk '{print $1}')
@@ -11,50 +12,113 @@ mount /dev/mapper/$VOLUME_GROUP_NAME /mnt
 cd /mnt
 
 # for snapper support
-mv @rootfs/ @
+if mv @rootfs/ @; then
+    echo "root subvol successfully moved to @"
+    sleep 1
+else
+    echo echo "root subvol moved failed"
+    exit 1
+fi
 
 # create subvolumes
-btrfs subvolume create @snapshots
-btrfs subvolume create @home
-btrfs subvolume create @tmp
-btrfs subvolume create @var
-# OPTIONAL
-btrfs subvolume create @opt
+subs=(
+    @snapshots
+    @home
+    @tmp
+    @var
+    @opt
+)
+for sub in "${subs[@]}"
+do 
+    echo "creating subvolume $sub..."
+    if ! btrfs subvolume create "$sub"; then
+        echo "Error: failed to create subvolume $sub"
+        exit 1
+done;
+echo "subvolumes created"
+sleep 1
 
 # mount root directory
-mount -o ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@ /dev/mapper/$VOLUME_GROUP_NAME /target
+if mount -o ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@ /dev/mapper/$VOLUME_GROUP_NAME /target; then
+    echo "successfully mounted @"
+    sleep 1
+else
+    echo "failed to mount @"
+    exit 1
+fi
 
-# create mount points
 cd /target
-mkdir .snapshots
-mkdir home
-mkdir tmp
-mkdir var
-# OPTIONAL
-mkdir opt
+# create mount points
+mountpoints=(
+    .snapshots
+    home
+    tmp
+    var
+    opt
+)
+for mountpoint in "${mountpoints[@]}"
+do
+    echo "creating mountpoint $mountpoint..."
+    if ! mkdir -p "$mountpoint"; then
+        echo "Error: failed to create mountpoint $mountpoint"
+        exit 1
+done
+echo "mountpoints created"
+sleep 1
 
 # mount subvolumes
-mount -o ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@snapshots /dev/mapper/$VOLUME_GROUP_NAME /target/.snapshots
-mount -o ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@home /dev/mapper/$VOLUME_GROUP_NAME /target/home
-mount -o ssd,noatime,nodatacow,space_cache=v2,commit=120,discard=async,subvol=@tmp /dev/mapper/$VOLUME_GROUP_NAME /target/tmp
-mount -o ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@var /dev/mapper/$VOLUME_GROUP_NAME /target/var
-# OPTIONAL /opt
-mount -o ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@opt /dev/mapper/$VOLUME_GROUP_NAME /target/opt
+mountoptions=(
+    ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@snapshots /dev/mapper/$VOLUME_GROUP_NAME /target/.snapshots
+    ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@home /dev/mapper/$VOLUME_GROUP_NAME /target/home
+    ssd,noatime,nodatacow,space_cache=v2,commit=120,discard=async,subvol=@tmp /dev/mapper/$VOLUME_GROUP_NAME /target/tmp
+    ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@var /dev/mapper/$VOLUME_GROUP_NAME /target/var
+    ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@opt /dev/mapper/$VOLUME_GROUP_NAME /target/opt
+)
+for mountoption in "${mountoptions[@]}"
+do
+    echo "mounting with $mountoption..."
+    if ! mount -o "$mountoption"; then
+        echo "Error: failed to mount with $mountoption"
+        exit 1
+done
+echo mounting subvolumes completed
+sleep 1
+
 # mount boot and efi partitions
-mount $boot boot
-mount $efi boot/efi
+if ! mount $boot boot; then
+        echo "Error: failed to mount $boot"
+        exit 1
+echo successfully mounted "$boot"
+sleep 1
+if ! mount $efi boot/efi; then
+        echo "Error: failed to mount $efi"
+        exit 1
+echo successfully mounted "$efi"
+sleep 1
 
 # remove original fstab entry for @
-sed '/$VOLUME_GROUP_NAME/d' etc/fstab
+sed '/$VOLUME_GROUP_NAME/d' etc/fstab >> /dev/null
 
-echo "/dev/mapper/$VOLUME_GROUP_NAME /             btrfs  ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@            0    1" >> etc/fstab
-echo "/dev/mapper/$VOLUME_GROUP_NAME /.snapshots   btrfs  ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@snapshots    0    2" >> etc/fstab
-echo "/dev/mapper/$VOLUME_GROUP_NAME /home         btrfs  ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@home        0    2" >> etc/fstab
-echo "/dev/mapper/$VOLUME_GROUP_NAME /tmp          btrfs  ssd,noatime,nodatacow,space_cache=v2,commit=120,discard=async,subvol=@tmp          0    2" >> etc/fstab
-echo "/dev/mapper/$VOLUME_GROUP_NAME /var          btrfs  ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@var          0    2" >> etc/fstab
-# OPTIONAL /opt
-echo "/dev/mapper/$VOLUME_GROUP_NAME /opt          btrfs  ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@opt          0    2" >> etc/fstab
+# writing /target/etc/fstab
+fstab_entries=(
+    "/dev/mapper/$VOLUME_GROUP_NAME /             btrfs  ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@           0    1"
+    "/dev/mapper/$VOLUME_GROUP_NAME /.snapshots   btrfs  ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@snapshots  0    2"
+    "/dev/mapper/$VOLUME_GROUP_NAME /home         btrfs  ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@home       0    2"
+    "/dev/mapper/$VOLUME_GROUP_NAME /tmp          btrfs  ssd,noatime,nodatacow,space_cache=v2,commit=120,discard=async,subvol=@tmp              0    2"
+    "/dev/mapper/$VOLUME_GROUP_NAME /var          btrfs  ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@var        0    2"
+    "/dev/mapper/$VOLUME_GROUP_NAME /opt          btrfs  ssd,noatime,space_cache=v2,commit=120,compress=zstd:1,discard=async,subvol=@opt        0    2"
+)
+for fstab_entry in "${fstab_entries[@]}"
+do
+    echo writing "$fstab_entry" into etc/fstab...
+    if ! echo "$fstab_entry" >> etc/fstab; then
+        echo "Error: failed to write into etc/fstab"
+        exit 1
+echo successfully added entries to etc/fstab
+sleep 1
 
 cd /
 umount /mnt
+echo successfully unmounted /mnt
+sleep 1
 exit
